254144050d44ae75c1f8a633126bbbecd648572e
Raja Yoken <rajayoken072@gmail.com>
2026-03-23 10:15:44 +0530

    Create bikecar.py

---
+++ bikecar.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the number if tyres:"))
+if(n%2==0):
+   if (n%4 != 0):
+      if n%4 == 2:
+         print("yes")
+      else :
+         print("no")
+   else:
+      print("no")
+else:
+   print("no")
